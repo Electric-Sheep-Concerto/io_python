47dc9159d8572d76b51bdfb369da1ddfe38e834f
sasanqua <78304794+sasanqua-dev@users.noreply.github.com>
2024-09-16 02:49:46 +0900

    Refactor constant.py to use environment variable for clientId

--- receiver.py
+++ receiver.py
@@ -1,14 +1,16 @@
 from lib.constant import clientId
 import paho.mqtt.client as mqtt
 from lib.player import play
+import random
 
 def on_connect(client, userdata, flag, rc):
-    client.publish("sheep/concerto", f"LOG> {str(client._client_id)}: listener connected")
+    client.publish("sheep/concerto", f"LOG> {str(client._client_id.decode())}: listener connected(RX)")
 
 def on_message(client, userdata, msg):
-    if "LOG>" in msg.payload.decode() or str(client._client_id) == clientId: # ログ出力
+    if "LOG>" in msg.payload.decode() or str(client._client_id.decode()) == clientId: # ログ出力
         return
     else:
+        print(f"LOG> {str(client._client_id)}: {msg.payload.decode()}")
         #### Play music
         play("sample.mp3")
         pass
@@ -18,7 +20,7 @@ def on_disconnect(client, userdata, rc):
         print("Unexpected disconnection.")
 
 if __name__ == "__main__":
-    client = mqtt.Client()
+    client = mqtt.Client(client_id=str(random.randint(1000, 9999)))
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
 
