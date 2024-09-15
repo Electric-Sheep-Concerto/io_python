c59bb1756b4475a8ea062cceca2311c3b71c884e
sasanqua <78304794+sasanqua-dev@users.noreply.github.com>
2024-09-15 18:50:22 +0900

    Refactor setup.sh to remove unnecessary Bluetooth device connection code and start tx.py and rx.py scripts

--- setup.sh
+++ setup.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-ble_mac="$BLE_MAC"
-if [ $BLE_MAC ]; then
-    sudo bluetoothctl <<
-    pair $ble_mac
-    trust $ble_mac
-    connect $ble_mac || true
-    exit
-EOF
-    echo "Connected to $ble_mac"
-else
-    echo "Please provide BLE_MAC"
-    exit 1
-fi
 nohup python tx.py &
 echo "Started tx.py"
 nohup python rx.py &
